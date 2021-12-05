e299d1b30ddcd993e38e7c5d08167831a0d28d2c
Ares240hz <80598627+Ares240hz@users.noreply.github.com>
2021-12-05 21:09:11 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-/* SCROLL TOP BTN */
+var myNavbar = document.getElementById("NavBar");
+myNavbar.style.display = "none";
+
 var mybutton = document.getElementById("upBtn");
     window.onscroll = function() {scrollFunction()};
     function scrollFunction() {
@@ -7,9 +9,9 @@ var mybutton = document.getElementById("upBtn");
         } else {
         mybutton.style.display = "none";
         }
-        if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
+        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
             $(".navbar").fadeIn();
-        } else {
+        }else {
             $(".navbar").fadeOut();
         }
     }
@@ -17,11 +19,11 @@ var mybutton = document.getElementById("upBtn");
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
     }
+
     var btncopy = document.querySelector('.js-copy');
     if(btncopy) {
     btncopy.addEventListener('click', docopy);
     }
-
     function docopy() {
     var range = document.createRange();
     var target = this.dataset.target;
@@ -32,26 +34,32 @@ var mybutton = document.getElementById("upBtn");
     selection.removeAllRanges();
     selection.addRange(range);
 
-try {
-    var result = document.execCommand('copy');
-    if (result) {
-    // La copie a réussi
-    btncopy.classList.add("clicked")
-    setTimeout(function(){
-        btncopy.classList.remove("clicked")
-        }, 1950);
+    try {
+        var result = document.execCommand('copy');
+        if (result) {
+        btncopy.classList.add("clicked")
+        setTimeout(function(){
+            btncopy.classList.remove("clicked")
+            }, 1950);
+        }
     }
-}
-catch(err) {
-    // Une erreur est surevnue lors de la tentative de copie
-    alert(err);
-}
+    catch(err) {
+        alert(err);
+    }
 
-selection = window.getSelection();
+    selection = window.getSelection();
 
-if (typeof selection.removeRange === 'function') {
-    selection.removeRange(range);
-} else if (typeof selection.removeAllRanges === 'function') {
-    selection.removeAllRanges();
-}
-}
+    if (typeof selection.removeRange === 'function') {
+        selection.removeRange(range);
+    } else if (typeof selection.removeAllRanges === 'function') {
+        selection.removeAllRanges();
+    }
+    }
+
+    function getCookie(c_name) {
+        return localStorage.getItem(c_name);
+    }
+
+    function setCookie(c_name, value, expiredays) {
+        return localStorage.setItem(c_name, value);
+    }
